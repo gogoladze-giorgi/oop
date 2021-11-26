a41d2f5cfe5d8895f555cf9623b95217ac7b41c7
Giorgi Gogoladze <gogoladzeg321@gmail.com>
2021-11-26 17:10:07 +0400

    commited

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -1,7 +1,6 @@
-'use strict';
 
-/*
-///////////////////////////////////////
+
+
 // Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
   // Instance properties
@@ -83,24 +82,15 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
-*/
 
-///////////////////////////////////////
+
+
 // Coding Challenge #1
 
-/* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
 
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
 
-GOOD LUCK 😀
-*/
 
-/*
+
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -125,13 +115,7 @@ bmw.brake();
 bmw.accelerate();
 
 
-///////////////////////////////////////
-// ES6 Classes
 
-// Class expression
-// const PersonCl = class {}
-
-// Class declaration
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -234,21 +218,13 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
-*/
+
 
 ///////////////////////////////////////
 // Coding Challenge #2
 
-/* 
-1. Re-create challenge 1, but this time using an ES6 class;
-2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
-3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
-4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
 
-DATA CAR 1: 'Ford' going at 120 km/h
 
-GOOD LUCK 😀
-*/
 
 /*
 class CarCl {
